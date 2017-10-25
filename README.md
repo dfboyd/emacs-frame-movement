e4cb3cdfd153a943c2f96e1905859cfbe4702b62
Daniel Boyd <dboyd@2u.com>
2017-10-25 17:34:39 -0400

    Add README, add code

---
+++ README.md
@@ -0,0 +1,10 @@
+# frame-movement.el
+
+These commands let you move from frame to frame on the display
+according to their visible position.  Moving to the "next" frame means
+moving to the next frame to the right; the "previous" frame is the
+next frame on the left.
+
+It does exactly what's asked for in:
+
+https://emacs.stackexchange.com/questions/29600/switch-to-a-neighboring-frame
---
+++ frame-movement.el
@@ -0,0 +1,49 @@
+;; frame-movement:
+;; switch Emacs frames left/right
+
+(provide 'frame-movement)
+
+(defun frame-movement/frame-rowmajor (frame-a frame-b)
+  (let* ((frame-a-pos (frame-position frame-a))
+	 (ax (car frame-a-pos))
+	 (ay (cdr frame-a-pos))
+	 (frame-b-pos (frame-position frame-b))
+	 (bx (car frame-b-pos))
+	 (by (cdr frame-b-pos)))
+    (and (<= ay by)
+	 (<= ax bx))))
+
+(defun frame-movement/sort-by-position (framelist)
+  (sort framelist #'frame-movement/frame-rowmajor))
+
+(defun frame-movement/find-next-frame-recursive (frame first-frame frame-list)
+  (cond ((null frame-list) first-frame)
+	((eq frame (car frame-list)) (if (null (cdr frame-list))
+					first-frame
+				      (car (cdr frame-list))))
+	(t (frame-movement/find-next-frame-recursive frame
+					     first-frame
+					     (cdr frame-list)))))
+
+(defun frame-movement/select-next-frame ()
+  (interactive)
+  (let* ((sorted-frames (frame-movement/sort-by-position (visible-frame-list)))
+	 (zero-frame (car sorted-frames))
+	 (target-frame (frame-movement/find-next-frame-recursive (selected-frame)
+							 zero-frame
+							 sorted-frames)))
+    (select-frame-set-input-focus target-frame)))
+
+(defun frame-movement/select-prev-frame ()
+  (interactive)
+  (let* ((sorted-frames (reverse (frame-movement/sort-by-position
+				  (visible-frame-list))))
+	 (zero-frame (car sorted-frames))
+	 (target-frame (frame-movement/find-next-frame-recursive (selected-frame)
+							 zero-frame
+							 sorted-frames)))
+    (select-frame-set-input-focus target-frame)))
+
+(global-set-key (kbd "C-x 5 n") 'frame-movement/select-next-frame)
+(global-set-key (kbd "C-x 5 p") 'frame-movement/select-prev-frame)
+
