11b4992aa131bba25f6e55c1f1c3442ecf63e1aa
Daniel F Boyd <dfboyd@gmail.com>
2024-06-12 18:58:58 -0400

    Replace select-frame-offset with find-next-frame-recursive
    This is a checkin of the existing code I have on my desktop, to get
    Git up to date before I check in a fix I made for frame-rowmajor.
    
    In retrospect I don't know why I made this code change; it may have
    been because I thought select-frame-offset wasn't workking right,
    because sometimes the cursor would go to the wrong next frame.
    
    This change removes the `arg` for goto-next-frame etc, so you can only
    go to the adjacent frame.

--- frame-movement.el
+++ frame-movement.el
@@ -4,53 +4,50 @@
 (provide 'frame-movement)
 
 (defun frame-movement/frame-rowmajor (frame-a frame-b)
-  "Given two frame objects, dig out their X and Y coordinates,
-and compare them in row-major order.  The first one is whichever
-frame is closer to the top of the screen; of two frames at the
-same height, the first one is on the left."
   (let* ((frame-a-pos (frame-position frame-a))
 	 (ax (car frame-a-pos))
 	 (ay (cdr frame-a-pos))
 	 (frame-b-pos (frame-position frame-b))
 	 (bx (car frame-b-pos))
 	 (by (cdr frame-b-pos)))
-    (and (<= ay by)
-	 (< ax bx))))
+    (and (<= ax bx)
+	 (<= ay by))))
 
 (defun frame-movement/sort-by-position (framelist)
   (sort framelist #'frame-movement/frame-rowmajor))
 
-(defun frame-movement/select-frame-offset (offset)
-  "Find the frame that is OFFSET positions away from the currently-selected
-frame in the list of all visible frames sorted by their X/Y positions,
-and select-frame-set-input-focus it."
-  (let* ((sorted-frames-vector (vconcat
-				(frame-movement/sort-by-position
-				 (visible-frame-list))))
-	 (current-frame-index (seq-position sorted-frames-vector 
-					    (selected-frame)))
-	 (target-index (mod (+ current-frame-index offset) 
-			    (length sorted-frames-vector)))
-	 (target-frame (elt sorted-frames-vector target-index)))
+(defun frame-movement/find-next-frame-recursive (frame first-frame frame-list)
+  (cond ((null frame-list) first-frame)
+	((eq frame (car frame-list)) (if (null (cdr frame-list))
+					 first-frame
+				       (car (cdr frame-list))))
+	(t (frame-movement/find-next-frame-recursive frame
+						     first-frame
+						     (cdr frame-list)))))
+
+(defun frame-movement/goto-next-frame ()
+  (interactive)
+  (let* ((sorted-frames (frame-movement/sort-by-position (visible-frame-list)))
+	 (zero-frame (car sorted-frames))
+	 (target-frame
+	  (frame-movement/find-next-frame-recursive (selected-frame)
+						    zero-frame
+						    sorted-frames)))
     (select-frame-set-input-focus target-frame)))
 
-(defun frame-movement/select-next-frame (arg)
-  "Switch to the ARG'th rightward adacent visible frame 
-at the same height as the currently-selected frame.
-If there are no more frames to the right in the same row,
-go to the rightmost frame in the next row.  
-Otherwise, wrap around to the highest leftmost frame."
-  (interactive "p")
-  (frame-movement/select-frame-offset arg))
+(defun frame-movement/goto-prev-frame ()
+  (interactive)
+  (let* ((sorted-frames (reverse (frame-movement/sort-by-position
+				  (visible-frame-list))))
+	 (zero-frame (car sorted-frames))
+	 (target-frame
+	  (frame-movement/find-next-frame-recursive (selected-frame)
+						    zero-frame
+						    sorted-frames)))
+    (select-frame-set-input-focus target-frame)))
 
-(defun frame-movement/select-prev-frame (arg)
-  "Switch to the ARG'th leftward adacent visible frame 
-at the same height as the currently-selected frame.
-If there are no more frames to the left in the same row,
-go to the rightmost frame in the previous row.  
-Otherwise, wrap around to the lowest rightmost frame."
-  (interactive "p")
-  (frame-movement/select-frame-offset (- arg)))
+(global-set-key (kbd "C-x [") 'frame-movement/goto-next-frame)
+(global-set-key (kbd "C-x ]") 'frame-movement/goto-prev-frame)
+(global-set-key (kbd "C-x 5 n") 'frame-movement/goto-next-frame)
+(global-set-key (kbd "C-x 5 p") 'frame-movement/goto-prev-frame)
 
-(global-set-key (kbd "C-x 5 n") 'frame-movement/select-next-frame)
-(global-set-key (kbd "C-x 5 p") 'frame-movement/select-prev-frame)
